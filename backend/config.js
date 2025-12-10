673a37d172e06ecc0042845fb4240e911c985d2b
SupunCharuka <supuncharuka.dev@gmail.com>
2025-12-10 11:40:32 +0530

    feat: integrate mongoose for MongoDB connection and update configuration

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,8 @@
-export const PORT = Number(process.env.PORT) || 5000;
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const PORT = Number(process.env.PORT);
+
+export const mongoDBURL = process.env.MONGODB_URL;
+
+export const FRONTEND_URL = process.env.FRONTEND_URL;
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,29 @@
 import express from 'express';
 import cors from 'cors';
-import { PORT } from './config.js';
+import mongoose from 'mongoose';
+import { PORT, mongoDBURL, FRONTEND_URL } from './config.js';
+
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use(express.json());
+
+// CORS configuration
+app.use(cors({
+  origin: FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  }); 
